27ad2cff1db5d3efd15bd3e6e74c513ad63554ea
dinbruno <brunodinosantos@outlook.com>
2023-07-22 13:45:51 -0300

    feat: added swagger docs

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,33 +1,50 @@
 import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse, ApiBody, ApiParam } from '@nestjs/swagger';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/createTask.dto';
 import { UpdateTaskDto } from './dto/updateTask.dto';
 
+@ApiTags('tasks')
 @Controller('tasks')
 export class TasksController {
     constructor(private readonly tasksService: TasksService) {}
-  
+
     @Post()
+    @ApiOperation({ summary: 'Create task' })
+    @ApiResponse({ status: 201, description: 'The task has been successfully created.'})
+    @ApiBody({ type: CreateTaskDto })
     async create(@Body() createTaskDto: CreateTaskDto) {
       return this.tasksService.create(createTaskDto);
     }
-  
+
     @Get()
+    @ApiOperation({ summary: 'List all tasks' })
+    @ApiResponse({ status: 200, description: 'Successful operation.' })
     async findAll() {
       return this.tasksService.findAll();
     }
-  
+
     @Get(':id')
+    @ApiOperation({ summary: 'Get a task by id' })
+    @ApiResponse({ status: 200, description: 'Successful operation.' })
+    @ApiParam({ name: 'id', required: true, description: 'ID of the task' })
     async findOne(@Param('id') id: string) {
       return this.tasksService.findOne(id);
     }
-  
+
     @Put(':id')
+    @ApiOperation({ summary: 'Update a task by id' })
+    @ApiResponse({ status: 200, description: 'The task has been successfully updated.'})
+    @ApiParam({ name: 'id', required: true, description: 'ID of the task' })
+    @ApiBody({ type: UpdateTaskDto })
     async update(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
       return this.tasksService.update(id, updateTaskDto);
     }
-  
+
     @Delete(':id')
+    @ApiOperation({ summary: 'Delete a task by id' })
+    @ApiResponse({ status: 200, description: 'The task has been successfully deleted.'})
+    @ApiParam({ name: 'id', required: true, description: 'ID of the task' })
     async delete(@Param('id') id: string) {
       return this.tasksService.delete(id);
     }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,33 +1,50 @@
 import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse, ApiBody, ApiParam } from '@nestjs/swagger';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/createUser.dto';
 import { UpdateUserDto } from './dto/updateUser.dto';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create user' })
+  @ApiResponse({ status: 201, description: 'The user has been successfully created.'})
+  @ApiBody({ type: CreateUserDto })
   async create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'List all users' })
+  @ApiResponse({ status: 200, description: 'Successful operation.' })
   async findAll() {
     return this.usersService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get a user by id' })
+  @ApiResponse({ status: 200, description: 'Successful operation.' })
+  @ApiParam({ name: 'id', required: true, description: 'ID of the user' })
   async findOne(@Param('id') id: string) {
     return this.usersService.findOne(id);
   }
 
   @Put(':id')
+  @ApiOperation({ summary: 'Update a user by id' })
+  @ApiResponse({ status: 200, description: 'The user has been successfully updated.'})
+  @ApiParam({ name: 'id', required: true, description: 'ID of the user' })
+  @ApiBody({ type: UpdateUserDto })
   async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete a user by id' })
+  @ApiResponse({ status: 200, description: 'The user has been successfully deleted.'})
+  @ApiParam({ name: 'id', required: true, description: 'ID of the user' })
   async delete(@Param('id') id: string) {
     return this.usersService.delete(id);
   }
