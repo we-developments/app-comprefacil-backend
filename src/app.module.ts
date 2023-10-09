3a87f16ebfc65129b33279cd8116a8d4153b0bb7
LezierF <lezier.f_@hotmail.com>
2023-10-09 00:40:42 -0300

    feat: chamnando positionsmodule no main

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,17 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { TasksModule } from './tasks/tasks.module';
+import { PositionsModule } from './positions/positions.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://brunodin:tlwr541g@cluster0.wyvff7p.mongodb.net/?retryWrites=true&w=majority'),
+    MongooseModule.forRoot(
+      'mongodb+srv://brunodin:tlwr541g@cluster0.wyvff7p.mongodb.net/?retryWrites=true&w=majority',
+    ),
     UsersModule,
     TasksModule,
+    PositionsModule,
+    PositionsModule,
   ],
   controllers: [],
   providers: [],
