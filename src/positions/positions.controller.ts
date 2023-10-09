953d3a0819c42ec0cfc87fb7808f3d210af88fd8
LezierF <lezier.f_@hotmail.com>
2023-10-09 00:41:27 -0300

    feat: positions controller

---
+++ src/positions/positions.controller.ts
@@ -0,0 +1,71 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { PositionsService } from './positions.service';
+import {
+  ApiBody,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { createPositionDto } from './dto/createPosition.dto';
+import { UpdatePositionDto } from './dto/updatePosition.dto';
+
+@ApiTags('positions')
+@Controller('positions')
+export class PositionsController {
+  constructor(private readonly positionsService: PositionsService) {}
+
+  @Post()
+  @ApiOperation({ summary: 'Create task' })
+  @ApiResponse({
+    status: 201,
+    description: 'The task has been successfully created.',
+  })
+  @ApiBody({ type: createPositionDto })
+  async create(@Body() createPositionDto: createPositionDto) {
+    return this.positionsService.create(createPositionDto);
+  }
+
+  @Get()
+  @ApiOperation({ summary: 'Get all positions' })
+  @ApiResponse({ status: 200, description: 'Successful operation' })
+  async findAll() {
+    return this.positionsService.findAll();
+  }
+
+  @Get(':id')
+  @ApiOperation({ summary: 'Get a position by ID' })
+  @ApiResponse({ status: 200, description: 'Successful operation' })
+  @ApiParam({ name: 'id', required: true, description: 'ID of de position' })
+  async findOne(@Param('id') id: string) {
+    return this.positionsService.findOne(id);
+  }
+
+  @Put(':id')
+  @ApiOperation({ summary: 'Update position by id' })
+  async update(
+    @Param('id') id: string,
+    @Body() updatePositionDto: UpdatePositionDto,
+  ) {
+    return this.positionsService.update(id, updatePositionDto);
+  }
+
+  @Delete(':id')
+  @ApiOperation({ summary: 'Delete a position by id' })
+  @ApiResponse({
+    status: 200,
+    description: 'The position has been successfully deleted.',
+  })
+  @ApiParam({ name: 'id', required: true, description: 'ID of the position' })
+  async delete(@Param('id') id: string) {
+    return this.positionsService.delete(id);
+  }
+}
