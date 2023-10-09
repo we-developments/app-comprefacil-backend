49c39333a12654126acd8dd61d9aaac058542bb8
LezierF <lezier.f_@hotmail.com>
2023-10-09 00:41:11 -0300

    feat: anable cors para a api

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,20 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   const config = new DocumentBuilder()
     .setTitle('Compre fácil - backend')
     .setDescription('Usuários e tarefas')
     .setVersion('1.0')
     .addTag('compre-facil')
     .build();
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET, POST, PUT, DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
