a8f5bf051f45cfffba222c06a5f34c7a433cc2b7
LezierF <lezier.f_@hotmail.com>
2023-10-18 00:23:09 -0300

    fix: nome das description da @api

--- src/users/dto/createUserFirebase.dto.ts
+++ src/users/dto/createUserFirebase.dto.ts
@@ -3,7 +3,7 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserFirebaseDto {
   @IsString()
-  @ApiProperty({ description: 'The uid by firebase auth' })
+  @ApiProperty({ description: 'The name by firebase auth' })
   readonly name: string;
 
   @IsEmail()
@@ -11,22 +11,26 @@ export class CreateUserFirebaseDto {
   readonly email: string;
 
   @IsBoolean()
-  @ApiProperty({ description: 'The uid by firebase auth' })
+  @ApiProperty({ description: 'The isActive by firebase auth' })
   readonly isActive: string;
 
   @IsBoolean()
-  @ApiProperty({ description: 'The uid by firebase auth' })
+  @ApiProperty({ description: 'The isAdmin by firebase auth' })
   readonly isAdmin: string;
 
   @IsString()
-  @ApiProperty({ description: 'The uid by firebase auth' })
+  @ApiProperty({ description: 'The sector by firebase auth' })
   readonly sector: string;
 
   @IsString()
-  @ApiProperty({ description: 'The uid by firebase auth' })
+  @ApiProperty({ description: 'The jobTitle by firebase auth' })
   readonly jobTitle: string;
 
   @IsString()
-  @ApiProperty({ description: 'The uid by firebase auth' })
+  @ApiProperty({ description: 'The profilePicture by firebase auth' })
   readonly profilePicture: string;
+
+  @IsString()
+  @ApiProperty({ description: 'The _id by firebase auth' })
+  readonly _id: string;
 }
