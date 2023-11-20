2e7d1753b02906cf5c62dcc861a84ae6b8864e61
LezierF <lezier.f_@hotmail.com>
2023-11-20 00:33:09 -0300

    feat: chamando notification no module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { UsersModule } from './users/users.module';
 import { TasksModule } from './tasks/tasks.module';
 import { PositionsModule } from './positions/positions.module';
 import { SectorsModule } from './sectors/sectors.module';
+import { NotificationModule } from './notification/notification.module';
 
 @Module({
   imports: [
@@ -13,8 +14,8 @@ import { SectorsModule } from './sectors/sectors.module';
     UsersModule,
     TasksModule,
     PositionsModule,
-    PositionsModule,
     SectorsModule,
+    NotificationModule,
   ],
   controllers: [],
   providers: [],
